aae961b531344c1c2e6743e0100df27f7251532f
xemlock <xemlock@gmail.com>
2015-02-21 22:00:50 +0100

    rtrimed lines

--- bootstrap-navwalker.php
+++ bootstrap-navwalker.php
@@ -14,7 +14,7 @@
  */
 class themifier_bootstrap_navwalker extends Walker_Nav_Menu
 {
-    
+
     /**
      * @see Walker::start_lvl()
      * @since 3.0.0
@@ -27,7 +27,7 @@ class themifier_bootstrap_navwalker extends Walker_Nav_Menu
         $indent = str_repeat("\t", $depth);
         $output .= "\n$indent<ul role=\"menu\" class=\" dropdown-menu\">\n";
     }
-    
+
     /**
      * @see Walker::start_el()
      * @since 3.0.0
@@ -41,7 +41,7 @@ class themifier_bootstrap_navwalker extends Walker_Nav_Menu
     public function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0)
     {
         $indent = ($depth) ? str_repeat("\t", $depth) : '';
-        
+
         /**
          * Dividers, Headers or Disabled
          * =============================
@@ -59,32 +59,32 @@ class themifier_bootstrap_navwalker extends Walker_Nav_Menu
         } else if (strcasecmp($item->attr_title, 'disabled') == 0) {
             $output .= $indent . '<li role="presentation" class="disabled"><a href="#">' . esc_attr($item->title) . '</a>';
         } else {
-            
+
             $class_names = $value = '';
-            
+
             $classes   = empty($item->classes) ? array() : (array) $item->classes;
             $classes[] = 'menu-item-' . $item->ID;
-            
+
             $class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
-            
+
             if ($args->has_children)
                 $class_names .= ' dropdown';
-            
+
             if (in_array('current-menu-item', $classes))
                 $class_names .= ' active';
-            
+
             $class_names = $class_names ? ' class="' . esc_attr($class_names) . '"' : '';
-            
+
             $id = apply_filters('nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args);
             $id = $id ? ' id="' . esc_attr($id) . '"' : '';
-            
+
             $output .= $indent . '<li' . $id . $value . $class_names . '>';
-            
+
             $atts           = array();
             $atts['title']  = !empty($item->title) ? $item->title : '';
             $atts['target'] = !empty($item->target) ? $item->target : '';
             $atts['rel']    = !empty($item->xfn) ? $item->xfn : '';
-            
+
             // If item has_children add atts to a.
             if ($args->has_children && $depth === 0) {
                 $atts['href']          = '#';
@@ -94,9 +94,9 @@ class themifier_bootstrap_navwalker extends Walker_Nav_Menu
             } else {
                 $atts['href'] = !empty($item->url) ? $item->url : '';
             }
-            
+
             $atts = apply_filters('nav_menu_link_attributes', $atts, $item, $args);
-            
+
             $attributes = '';
             foreach ($atts as $attr => $value) {
                 if (!empty($value)) {
@@ -104,9 +104,9 @@ class themifier_bootstrap_navwalker extends Walker_Nav_Menu
                     $attributes .= ' ' . $attr . '="' . $value . '"';
                 }
             }
-            
+
             $item_output = $args->before;
-            
+
             /*
              * Glyphicons
              * ===========
@@ -118,15 +118,15 @@ class themifier_bootstrap_navwalker extends Walker_Nav_Menu
                 $item_output .= '<a' . $attributes . '><span class="glyphicon ' . esc_attr($item->attr_title) . '"></span>&nbsp;';
             else
                 $item_output .= '<a' . $attributes . '>';
-            
+
             $item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
             $item_output .= ($args->has_children && 0 === $depth) ? ' <span class="caret"></span></a>' : '</a>';
             $item_output .= $args->after;
-            
+
             $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
         }
     }
-    
+
     /**
      * Traverse elements to create list from elements.
      *
@@ -151,16 +151,16 @@ class themifier_bootstrap_navwalker extends Walker_Nav_Menu
     {
         if (!$element)
             return;
-        
+
         $id_field = $this->db_fields['id'];
-        
+
         // Display this element.
         if (is_object($args[0]))
             $args[0]->has_children = !empty($children_elements[$element->$id_field]);
-        
+
         parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
     }
-    
+
     /**
      * Menu Fallback
      * =============
@@ -175,38 +175,38 @@ class themifier_bootstrap_navwalker extends Walker_Nav_Menu
     public static function fallback($args)
     {
         if (current_user_can('manage_options')) {
-            
+
             extract($args);
-            
+
             $fb_output = null;
-            
+
             if ($container) {
                 $fb_output = '<' . $container;
-                
+
                 if ($container_id)
                     $fb_output .= ' id="' . $container_id . '"';
-                
+
                 if ($container_class)
                     $fb_output .= ' class="' . $container_class . '"';
-                
+
                 $fb_output .= '>';
             }
-            
+
             $fb_output .= '<ul';
-            
+
             if ($menu_id)
                 $fb_output .= ' id="' . $menu_id . '"';
-            
+
             if ($menu_class)
                 $fb_output .= ' class="' . $menu_class . '"';
-            
+
             $fb_output .= '>';
             $fb_output .= '<li><a href="' . admin_url('nav-menus.php') . '">Add a menu</a></li>';
             $fb_output .= '</ul>';
-            
+
             if ($container)
                 $fb_output .= '</' . $container . '>';
-            
+
             echo $fb_output;
         }
     }
